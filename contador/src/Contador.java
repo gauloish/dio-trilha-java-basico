a2347befb376c34de0d0852e7176f65656ac713e
gauloish <125079293+gauloish@users.noreply.github.com>
2024-07-08 22:17:16 -0300

    added contador class

---
+++ contador/src/Contador.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Contador {
+	public static void main(String[] args) {
+        Scanner terminal = new Scanner(System.in);
+
+		System.out.println("Digite o primeiro parâmetro");
+		int parametroUm = terminal.nextInt();
+
+		System.out.println("Digite o segundo parâmetro");
+		int parametroDois = terminal.nextInt();
+		
+		try {
+			contar(parametroUm, parametroDois);
+		} catch (ParametrosInvalidosException exception) {
+			exception.getMessage();
+		} finally {
+            terminal.close();
+        }
+	}
+
+	static void contar(int parametroUm, int parametroDois ) throws ParametrosInvalidosException {
+		int contagem = parametroDois - parametroUm;
+
+        if (contagem > 0) {
+            for (int numero = 1; numero <= contagem; numero++) {
+                System.out.printf("Imprimindo o número %d\n", numero);
+            }
+        }
+        else {
+            throw new ParametrosInvalidosException("O segundo parâmetro deve ser maior que o primeiro.");
+        }
+	}
+}
